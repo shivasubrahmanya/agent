9f170586abcf4fa4ca1e2bd56df0bc7cd3ba0a74
shivasubrahmanya <shivakc004@gmail.com>
2026-02-06 21:58:26 +0530

    finished setup

--- test_snov_domain.py
+++
@@ -1,7 +0,0 @@
-
-from services.snov_client import domain_search
-import json
-
-print("Testing Snov.io Domain Search for microsoft.com...")
-results = domain_search("microsoft.com", limit=3)
-print(json.dumps(results, indent=2))
